89bd1c56b1988ae555e83dacb989e928564279c7
Admin <erickafatimareignrligaray@gmail.com>
2026-05-11 23:17:53 +0800

    changes app / privacy and app / terms

--- app/terms/page.js
+++ app/terms/page.js
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Footer from "@/components/Footer"; // Ensure this path is correct
 import StaticHtmlPage from "@/components/StaticHtmlPage";
 
 export default function Page() {
